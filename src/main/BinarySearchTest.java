88d36ae5068a21fcc3ec77cd661f4050f65b3a48
ahmadiavin <ahmadiavin@gmail.com>
2020-07-02 21:28:06 -0400

    bonus finished

--- src/main/BinarySearchTest.java
+++ src/main/BinarySearchTest.java
@@ -1,4 +1,23 @@
 package main;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class BinarySearchTest {
+    int[] array;
+   @Before
+    public void init(){
+       array = new int[] {3,4,5,6,7,8,9};
+   }
+   @Test
+    public void emptyTest(){
+       assertEquals(true,true);
+   }
+   @Test
+    public void findSixReturnThree(){
+       int returnValue = BonusBinarySearch.binarySearch(array,6);
+       assertEquals(returnValue, 3);
+   }
 }
